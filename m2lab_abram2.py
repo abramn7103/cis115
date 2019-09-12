47545028a69dc97ee9fcb3d1f31411763bf2fb92
abramn7103 <36608501+abramn7103@users.noreply.github.com>
2019-09-12 10:46:27 -0400

    m2lab in class

---
+++ m2lab_abram2.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+M2T1
+Abram
+9/12/19
+"""
+#
+#Convert M2T1 into Python
+#
+#// M2LAB
+#// Noah Abram
+#// 9/12/19
+#
+#// Greet user by name,
+#// then tell them their age
+#// on their next birthday
+#
+#// Declarations
+#Declare String name
+#Declare Integer age
+#
+#// Ask for their name
+print( "What is your name?")
+#Input (name)
+name = input
+
+
+
+
+#// Ask for their age
+print( "What is your age?")
+#Input (age)
+age = int(input())
+
+#// Tell them (by name) how old they will be 
+#// on their birthday
+#set age = age + 1
+#Display "Well, ", name, " on your next birthday..."
+#Display "you will be ", age, "!"
+age = age + 1
+print("You will be " ,age, "on your next birthday!")
