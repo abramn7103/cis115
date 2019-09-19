d42f6f8166113896da8ab655a423b54f0d38111c
abramn7103 <36608501+abramn7103@users.noreply.github.com>
2019-09-19 10:40:27 -0400

    Add files via upload

---
+++ m2t1/m2t1_abram.py
@@ -0,0 +1,39 @@
+##// M2T1
+##// Noah Abram
+##// 9/17/2019
+##
+##// ask the user for two integers
+##// show the sum and the products
+##
+##// declare variables
+##Declare Integer integer1
+##Declare Integer integer2
+##
+##// ask user for first number
+##Display "What will your first number be?"
+print("what will your first number be?"
+      
+##Input integer1
+integer1 = int(input())
+      
+##// ask user for second number
+##Display "What will your second number be?"
+print("What will your second number be?"
+      
+##Input integer2
+integer2 = int(input())
+      
+##// display the sum of the numbers
+totalSum = integer1 + integer2
+
+##Display "The sum is: "
+      
+
+##Display sum
+##
+##// display the products of the numbers
+##Set product = integer1 * integer2
+##Display "The product is:"
+##Display product
+##
+##// 
